cf274461d8ac531caddfa7796da5a4cb1b80dcd7
akhundzadafatima45 <axundzadefatime.45@gmail.com>
2024-11-26 16:59:54 +0400

    updated

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("hamburger-icon").addEventListener("click", function () {
+    const pages = document.querySelector(".pages");
+    pages.classList.toggle("active"); 
+  });
+  
